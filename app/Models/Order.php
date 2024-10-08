cd7fa226aa9088389cdcc97cef8522baf9e04e87
Ahmed <ahmedahmadahmid73@gmail.com>
2024-10-08 10:17:48 +0200

    make tables

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'customer_id',
+        'user_id',
+        'branch_id',
+        'amount',
+        'order_status',
+        'order_type',
+        'payment_status',
+        'total_tax',
+        'total_discount',
+        'address',
+        // address {phone, floor, road, address, latitude, longitude}
+    ];
+}
