a3f2bc307af800384f46af7b8837f0904010973a
Ahmed <ahmedahmadahmid73@gmail.com>
2024-10-31 11:54:47 +0200

    commit

--- routes/admin.php
+++ routes/admin.php
@@ -203,7 +203,7 @@ Route::middleware(['auth:sanctum', 'IsAdmin'])->group(function(){
             Route::delete('/delete/{id}', 'delete');
         });
         
-        Route::controller(SettingController::class)->prefix('discount')->group(function(){
+        Route::controller(SettingController::class)->group(function(){
             Route::get('/view_time_cancel', 'view_time_cancel_order');
             Route::post('/update_time_cancel', 'update_time_cancel_order');
         });
