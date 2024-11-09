18c77fea6d5b10c8b40e9b6d9b1f00bad2703c9a
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-09 10:33:57 +0200

    make profile of delivery

--- routes/delivery.php
+++ routes/delivery.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\delivery\order\OrderController;
 
+use App\Http\Controllers\api\delivery\profile\ProfileController;
+
 Route::middleware(['auth:sanctum', 'IsDelivery'])->group(function(){
     Route::controller(OrderController::class)->prefix('orders')->group(function(){ 
         Route::get('/', 'orders');
         Route::put('/status', 'status');
+    });
+    
+    Route::controller(ProfileController::class)->prefix('profile')->group(function(){ 
+        Route::get('/profile_data', 'profile_data');
+        Route::post('/update', 'update_profile');
     });
 });
