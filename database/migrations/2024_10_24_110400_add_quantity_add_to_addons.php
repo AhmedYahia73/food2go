f93718c71ea1cd36ac8b6795aba1c1c67eae6908
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-10 11:43:04 +0200

    commit

--- database/migrations/2024_10_24_110400_add_quantity_add_to_addons.php
+++ database/migrations/2024_10_24_110400_add_quantity_add_to_addons.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('addons', function (Blueprint $table) {
-            $table->boolean('quantity_add');
+            $table->boolean('quantity_add')->default(1);
         });
     }
 
