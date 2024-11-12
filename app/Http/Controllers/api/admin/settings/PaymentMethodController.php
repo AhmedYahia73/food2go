f4243eba9642db187300b7d789bd1e211f28932f
Ahmed <ahmedahmadahmid73@gmail.com>
2024-11-12 13:12:02 +0200

    make payment method at settings at admin dashboard

--- app/Http/Controllers/api/admin/settings/PaymentMethodController.php
+++ app/Http/Controllers/api/admin/settings/PaymentMethodController.php
@@ -31,16 +31,44 @@ class PaymentMethodController extends Controller
     public function create(PaymentMethodRequest $request){
         $paymentMethodRequest = $request->only($this->paymentMethodRequest);
         if ($request->logo) {
-            $image_path = 
+            $image_path = $this->upload($request, 'logo', 'admin/settings/payment_methods');
             $paymentMethodRequest['logo'] = $image_path;
         }
+        $payment_method = $this->payment_methods
+        ->create($paymentMethodRequest);
+
+        return response()->json([
+            'payment_method' => $payment_method
+        ]);
     }
 
     public function modify(PaymentMethodRequest $request, $id){
-        
+        $paymentMethodRequest = $request->only($this->paymentMethodRequest);
+        $payment_method = $this->payment_methods
+        ->where('id', $id)
+        ->first();
+        if (!is_string($request->logo)) {
+            $image_path = $this->upload($request, 'logo', 'admin/settings/payment_methods');
+            $paymentMethodRequest['logo'] = $image_path;
+            $this->deleteImage($payment_method->logo);
+        }
+        $payment_method
+        ->update($paymentMethodRequest);
+
+        return response()->json([
+            'payment_method' => $payment_method
+        ]);
     }
 
     public function delete($id){
-        
+        $payment_method = $this->payment_methods
+        ->where('id', $id)
+        ->first();
+        $this->deleteImage($payment_method->logo);
+        $payment_method->delete();
+
+        return response()->json([
+            'success' => 'You delete data success'
+        ]);
     }
 }
