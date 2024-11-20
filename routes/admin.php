191c1c19525422297e6e00ff7e33b6300984428d
Ahmed <Wegostationdev@gmail.com>
2024-11-20 15:27:27 +0200

    commit

--- routes/admin.php
+++ routes/admin.php
@@ -49,7 +49,7 @@ use App\Http\Controllers\api\admin\settings\PaymentMethodController;
 Route::middleware(['auth:sanctum', 'IsAdmin'])->group(function(){
     Route::controller(OrderController::class)->prefix('order')->group(function(){
         Route::get('/', 'orders');
-        Route::get('/filter', 'order_filter');
+        Route::post('/filter', 'order_filter');
         Route::get('/branches', 'branches');
         Route::get('/order/{id}', 'order');
         Route::get('/invoice/{id}', 'invoice');
