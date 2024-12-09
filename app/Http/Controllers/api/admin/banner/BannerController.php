d2b985f42fa6367886a4bcb1c42f77e28d6d28e9
Ahmed <Wegostationdev@gmail.com>
2024-12-09 16:57:15 +0200

    commit

--- app/Http/Controllers/api/admin/banner/BannerController.php
+++ app/Http/Controllers/api/admin/banner/BannerController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\admin\banner\BannerRequest;
 
 use App\Models\Banner;
 use App\Models\Translation;
+use App\Models\TranslationTbl;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Deal;
@@ -16,7 +17,8 @@ use App\Models\Deal;
 class BannerController extends Controller
 {
     public function __construct(private Banner $banner, private Translation $translations,
-    private Category $categories, private Product $products, private Deal $deals){}
+    private Category $categories, private Product $products, private Deal $deals,
+    private TranslationTbl $translation_tbl){}
     protected $bannerRequest = [
         'order',
         'category_id',
@@ -55,14 +57,29 @@ class BannerController extends Controller
         // order, category_id, product_id, deal_id
         // images [{translation_id, tranlation_name, image}]
         $bannerRequest = $request->only($this->bannerRequest);
-        foreach ($request->images as $item) {
-            if (!is_string($item['image'])) {
-                $image_path = $this->uploadFile($item['image'], 'admin/banner/image');
-                $bannerRequest['image'] = $image_path;
+        $banner = [];
+        $image_path = null;
+        foreach ($request->images as $key => $item) {
+            if ($key == 0) {
+                if (!is_string($item['image'])) {
+                    $image_path = $this->uploadFile($item['image'], 'admin/banner/image');
+                    $bannerRequest['image'] = $image_path;
+                }
+                $bannerRequest['translation_id'] = $item['translation_id'];
+                $banner = $this->banner
+                ->create($bannerRequest);
             }
-            $bannerRequest['translation_id'] = $item['translation_id'];
-            $this->banner
-            ->create($bannerRequest);
+            else{
+                if (!is_string($item['image'])) {
+                    $image_translation_path = $this->uploadFile($item['image'], 'admin/banner/image');
+                    $bannerRequest['image'] = $image_translation_path;
+                }
+                $banner->translations()->create([
+                    'locale' => $item['tranlation_name'],
+                    'key' => $image_path,
+                    'value' => $image_translation_path
+                ]);
+            }
         }
 
         return response()->json([
@@ -79,14 +96,36 @@ class BannerController extends Controller
         $banner = $this->banner
         ->where('id', $id)
         ->first();
-        foreach ($request->images as $item) {
-            if (!is_string($item['image'])) {
-                $image_path = $this->uploadFile($item['image'], 'admin/banner/image');
-                $bannerRequest['image'] = $image_path;
-                $this->deleteImage($banner->image);
+        $banner_image = $banner->image;
+        $image_path = null;
+        foreach ($request->images as $key => $item) {
+            if ($key == 0) {
+                if (!is_string($item['image'])) {
+                    $image_path = $this->uploadFile($item['image'], 'admin/banner/image');
+                    $bannerRequest['image'] = $image_path;
+                    $this->deleteImage($banner->image);
+                }
+                $bannerRequest['translation_id'] = $item['translation_id'];
+                $banner->update($bannerRequest);
             }
-            $bannerRequest['translation_id'] = $item['translation_id'];
-            $banner->update($bannerRequest);
+            else{
+                if (!is_string($item['image'])) {
+                    $translation_tbl = $this->translation_tbl
+                    ->where('key', $banner_image)
+                    ->where('translatable_id', $banner->id)
+                    ->where('locale', $item['tranlation_name'])
+                    ->first();
+                    $this->deleteImage($translation_tbl->value);
+                    if (!empty($translation_tbl)) {
+                        $translation_tbl->delete();
+                    }
+                    $banner->translations()->create([
+                        'locale' => $item['tranlation_name'],
+                        'key' => $banner->image,
+                        'value' => $item['image']
+                    ]);
+                }
+            }
         }
 
         return response()->json([
@@ -100,6 +139,16 @@ class BannerController extends Controller
         ->where('id', $id)
         ->first();
         $this->deleteImage($banner->image);
+        $translation_tbl = $this->translation_tbl
+        ->where('key', $banner->image)
+        ->where('translatable_id', $banner->id)
+        ->get();
+        foreach ($translation_tbl as $item) {
+            $this->deleteImage($item->value);
+            if (!empty($translation_tbl)) {
+                $item->delete();
+            }
+        }
         $banner->delete();
 
         return response()->json([
