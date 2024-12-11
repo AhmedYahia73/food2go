099aa357aabdbff97e04447511e779f0a146ab6c
Ahmed <Wegostationdev@gmail.com>
2024-12-11 15:34:47 +0200

    commit

--- app/Http/Controllers/api/admin/deal_order/DealOrderController.php
+++ app/Http/Controllers/api/admin/deal_order/DealOrderController.php
@@ -34,7 +34,7 @@ class DealOrderController extends Controller
         $code = $request->code;
         $deals = $this->deals
         ->whereHas('deal_customer', function($query) use ($nowSubThreeMinutes, $code){
-            $query
+            $query->where('deal_user.created_at', '>=', $nowSubThreeMinutes)
             ->where('deal_user.ref_number', $code);
         })
         ->first();
