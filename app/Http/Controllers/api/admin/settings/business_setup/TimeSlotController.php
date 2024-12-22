2d682553d25617613c0ba04c8c3df9ecdf5fb0bf
Ahmed <Wegostationdev@gmail.com>
2024-12-22 10:12:58 +0200

    commit

--- app/Http/Controllers/api/admin/settings/business_setup/TimeSlotController.php
+++ app/Http/Controllers/api/admin/settings/business_setup/TimeSlotController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\api\admin\settings\business_setup;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Setting;
+
 class TimeSlotController extends Controller
 {
-    //
+    public function __construct(private Setting $settings){}
+
+    public function view(){
+
+    }
+
+    public function add(){
+
+    }
 }
