a747907a315c38059ebcf8d0d7975c1d94dc3384
Ahmed <Wegostationdev@gmail.com>
2025-01-09 11:25:45 +0200

    commit

--- app/Http/Controllers/api/customer/otp/OtpController.php
+++ app/Http/Controllers/api/customer/otp/OtpController.php
@@ -103,9 +103,7 @@ class OtpController extends Controller
         }
         $user = $this->user
         ->where('email', $request->email)
-        ->where('code', $request->code)
         ->orWhere('phone', $request->email)
-        ->where('code', $request->code)
         ->first();
         if (empty($user)) {
             return response()->json([
