4a89d3168a852ddfaf940e543903291544e723a8
Ahmed <Wegostationdev@gmail.com>
2025-02-19 12:12:51 +0200

    commit

--- app/Http/Controllers/api/admin/order/OrderController.php
+++ app/Http/Controllers/api/admin/order/OrderController.php
@@ -475,7 +475,7 @@ class OrderController extends Controller
         'created_at', 'updated_at', 'pos', 'delivery_id', 'address_id',
         'notes', 'coupon_discount', 'order_number', 'payment_method_id',
         'status', 'points', 'rejected_reason', 'transaction_id');
-        }, 'branch', 'delivery', 'payment_method', 'address', 'customer_cancel_reason'])
+        }, 'branch', 'delivery', 'payment_method', 'address'])
         ->find($id);
         $order->user->count_orders = count($order->user->orders);
         if (!empty($order->branch)) {
