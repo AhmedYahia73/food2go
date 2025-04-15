b01b429d0b976d1b401c65a5d56e1b9b93b70777
Ahmed <Wegostationdev@gmail.com>
2025-04-15 10:14:25 +0200

    commit

--- routes/captain.php
+++ routes/captain.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\api\captain_order\make_order\CaptainMakeOrderController
 Route::middleware(['auth:sanctum', 'IsCaptain'])->group(function(){
     Route::controller(CaptainMakeOrderController::class)
     ->group(function(){
-        Route::get('/lists', 'lists');
+        Route::get('/lists', 'lists')->withOutMiddleware(['auth:sanctum', 'IsCaptain']);
         Route::post('/make_order', 'order');
     });
 });
