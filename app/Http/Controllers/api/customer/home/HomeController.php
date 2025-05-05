22886d92b1f97a41f409046253480afbfede6322
Ahmed <Wegostationdev@gmail.com>
2025-05-05 14:42:48 +0200

    commit

--- app/Http/Controllers/api/customer/home/HomeController.php
+++ app/Http/Controllers/api/customer/home/HomeController.php
@@ -273,7 +273,7 @@ class HomeController extends Controller
         'variations' => function($query) use($locale){
             $query->withLocale($locale)
             ->with(['options' => function($query_option) use($locale){
-                $query_option->with(['extra' => function($query_extra) use($locale){
+                $query_option->with(['extra_pricing.extra' => function($query_extra) use($locale){
                     $query_extra->with('parent_extra')
                     ->withLocale($locale);
                 }])
