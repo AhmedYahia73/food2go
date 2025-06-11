a488884de8f59bb87ab0db55208c555cd0f6ea48
Ahmed <Wegostationdev@gmail.com>
2025-06-11 11:37:56 +0200

    commit

--- app/Http/Controllers/api/admin/ExtraGroup/ExtraGroupController.php
+++ app/Http/Controllers/api/admin/ExtraGroup/ExtraGroupController.php
@@ -56,7 +56,7 @@ class ExtraGroupController extends Controller
     public function create(Request $request){
         // https://bcknd.food2go.online/admin/extra_group/add
         //Key
-        // extra_names[tranlation_name, extra_name], pricing, group_id
+        // extra_names[tranlation_name, extra_name], pricing, group_id, min, max
         $validator = Validator::make($request->all(), [
             'extra_names' => 'required',
             'extra_names.*.tranlation_name' => 'required',
@@ -99,7 +99,7 @@ class ExtraGroupController extends Controller
     public function modify(Request $request, $id){
         // https://bcknd.food2go.online/admin/extra_group/update/{id}
         //Key
-        // extra_names[tranlation_name, extra_name], pricing, group_id
+        // extra_names[tranlation_name, extra_name], pricing, group_id, min, max
         $validator = Validator::make($request->all(), [
             'extra_names' => 'required',
             'extra_names.*.tranlation_name' => 'required',
