149ba451f24e9b1113e804752963adc5484ded8e
Ahmed <Wegostationdev@gmail.com>
2025-07-22 15:35:30 +0200

    commit

---
+++ app/Models/FinancialAccounting.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FinancialAccounting extends Model
+{ 
+    use HasFactory;
+
+    protected $fillable = [ 
+        'name',
+        'description',
+        'logo',
+        'status', 
+    ];
+    protected $appends = ['logo_link'];
+
+    public function getLogoLinkAttribute(){
+        return url('storage/' . $this->attributes['logo']);
+    }
+
+    
+}
---
+++ database/migrations/2025_07_22_132806_create_financial_branch_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_branch', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('financial_id')->nullable()->constrained('financial_accountings')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('branch_id')->nullable()->constrained('branches')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_branch');
+    }
+};
