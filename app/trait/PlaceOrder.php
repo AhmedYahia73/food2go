6e5c7e0d767b9f3437b6857944f364b1c3b1c890
Ahmed <Wegostationdev@gmail.com>
2025-08-14 14:09:12 +0200

    commit

--- app/trait/PlaceOrder.php
+++ app/trait/PlaceOrder.php
@@ -731,6 +731,8 @@ trait PlaceOrder
 
     public function takeaway_order_format($order){
         $order_data = [];
+            return $order;
+
         foreach ($order->order_details ?? $order as $key => $item) {
             return $item;
             $product = $item->product[0]->product;
