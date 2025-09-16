b611270a0345df78ab149ad80016095b563ca832
Ahmed <Wegostationdev@gmail.com>
2025-09-16 10:39:33 +0200

    commit

--- app/Http/Controllers/api/captain_order/make_order/CaptainMakeOrderController.php
+++ app/Http/Controllers/api/captain_order/make_order/CaptainMakeOrderController.php
@@ -149,9 +149,23 @@ class CaptainMakeOrderController extends Controller
         $products = ProductResource::collection($products);
 
         $products = collect($products)->map(function ($item) {
-            return $item;
-        });
-
+            return [
+                'id' => $item->id,
+                'taxes' => $item->taxes,
+                'name' => $item->name,
+                'description' => $item->description,
+                'category_id' => $item->category_id,
+                'sub_category_id' => $item->sub_category_id,
+                'price' => $item->price,
+                'price_after_discount' => $item->price_after_discount,
+                'price_after_tax' => $item->price_after_tax,
+                'image_link' => $item->image_link,
+                'allExtras' => $item?->allExtras->select('id', 'price_after_discount', 'price_after_tax', 'name', 'price'),
+                'addons' => $item?->addons?->select('id', 'name', 'price', 'price_after_tax', 'quantity_add'),
+                'excludes' => $item?->excludes?->select('id', 'name'),
+                'variations' => $item?->variations?->select('id', 'name', 'type', 'min', 'max', 'required', 'options')
+            ];
+        }); 
 
         return response()->json([
             'categories' => $categories,
