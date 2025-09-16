b1df376df39e65a18c94845455f68d07c19d7c4d
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-09-16 22:04:09 +0200

    commit

--- routes/admin.php
+++ routes/admin.php
@@ -118,7 +118,7 @@ Route::middleware(['auth:sanctum', 'IsAdmin'])->group(function(){
     ->prefix('wasted')->group(function(){
         Route::get('/', 'view');
         Route::get('/item/{id}', 'wested');
-        Route::post('/status/{id}', 'status');
+        Route::put('/status/{id}', 'status');
         Route::post('/add', 'create'); 
         Route::post('/update/{id}', 'modify'); 
     });
