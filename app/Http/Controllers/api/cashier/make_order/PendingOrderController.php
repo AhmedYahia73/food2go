00a0104919758d7d1e2435ca7889dfbe34d2f22b
Ahmed <Wegostationdev@gmail.com>
2025-09-20 11:59:12 +0200

    commit

--- app/Http/Controllers/api/cashier/make_order/PendingOrderController.php
+++ app/Http/Controllers/api/cashier/make_order/PendingOrderController.php
@@ -49,12 +49,22 @@ class PendingOrderController extends Controller
         'order_status', 'order_type', 'payment_status', 'total_tax', 'total_discount',
         'created_at', 'updated_at', 'pos', 'delivery_id', 'address_id',
         'notes', 'coupon_discount', 'order_number', 'payment_method_id', 
-        'status', 'points', 'rejected_reason', 'transaction_id')
+        'status', 'points', 'rejected_reason', 'transaction_id',
+		'order_details')
         ->where('branch_id', $request->user()->branch_id)
         ->orderByDesc('id')
         ->whereBetween('created_at', [$start, $end])
         ->where('order_active', 0)
-        ->get();
+        ->get()
+		->map(function($item){
+			return [
+				'id' => $item->id,
+				'amount' => $item->amount,
+				'order_details' => $item->order_details,
+				'order_number' => $item->order_number,
+				'notes' => $item->notes,
+			];
+		});
 
         return response()->json([
             'all_orders' => $all_orders
@@ -67,19 +77,23 @@ class PendingOrderController extends Controller
         'order_status', 'order_type', 'payment_status', 'total_tax', 'total_discount',
         'created_at', 'updated_at', 'pos', 'delivery_id', 'address_id',
         'notes', 'coupon_discount', 'order_number', 'payment_method_id', 
-        'status', 'points', 'rejected_reason', 'transaction_id')
+        'status', 'points', 'rejected_reason', 'transaction_id', 'order_details')
         ->where('id', $id)
         ->where('order_active', 0) 
         ->first();
         // if(){
 
-        // }
-        $this->order
+        // } 
+        $this->orders
         ->where('id', $id)
         ->delete();
 
         return response()->json([
-            'order' => $order
-        ]);
+			'id' => $order->id,
+			'amount' => $order->amount,
+			'order_details' => $order->order_details,
+			'order_number' => $order->order_number,
+			'notes' => $order->notes,
+		]);
     }
 }
