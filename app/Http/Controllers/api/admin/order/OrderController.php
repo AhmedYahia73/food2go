d2cf3f425c42e69c4bcd2cf21dec3ca4da3ed743
Ahmed <Wegostationdev@gmail.com>
2025-10-07 09:54:22 +0200

    commit

--- app/Http/Controllers/api/admin/order/OrderController.php
+++ app/Http/Controllers/api/admin/order/OrderController.php
@@ -625,7 +625,7 @@ class OrderController extends Controller
     public function order_details(Request $request){
         // https://bcknd.food2go.online/admin/order 
         $validator = Validator::make($request->all(), [
-            'order_status' => 'required|in:delivery,confirmed,processing,out_for_delivery,delivered,returned,faild_to_deliver,canceled,scheduled,refund',
+            'order_status' => 'required|in:pending,delivery,confirmed,processing,out_for_delivery,delivered,returned,faild_to_deliver,canceled,scheduled,refund',
         ]);
         if ($validator->fails()) { // if Validate Make Error Return Message Error
             return response()->json([
