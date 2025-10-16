61008d55b1659800fca4f990a7dd86e452623915
Ahmed <Wegostationdev@gmail.com>
2025-10-16 11:35:05 +0200

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -56,7 +56,9 @@ class Product extends Model
     }
 
     public function getImageLinkAttribute(){
-        return url('storage/' . $this->attributes['image']);
+        if(isset($this->attributes['image'])){
+            return url('storage/' . $this->attributes['image']);
+        }
     }
 
     public function category(){
