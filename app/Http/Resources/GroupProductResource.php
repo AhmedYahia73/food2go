e36475afcdba7869fdfdf2be66e686f59c8b8cdd
Ahmed <Wegostationdev@gmail.com>
2025-10-23 14:10:23 +0200

    commit

--- app/Http/Resources/GroupProductResource.php
+++ app/Http/Resources/GroupProductResource.php
@@ -14,14 +14,14 @@ class GroupProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     { 
+        $locale = app()->getLocale();
         return [
             'id' => $this->id,
-            'name' => $this->translations->where('key', $this->name)->first()?->value ?? $this->name,
+            'name' => $this->translations->where('key', $this->name)
+            ->where("locale", $locale)->first()?->value ?? $this->name,
             'status' => $this->status,
-            'products' => ProductUpsalingResource::collection($this->whenLoaded('products'))
-            ->additional([
-                'locale' => app()->getLocale()
-            ]),
+            'products' => ProductUpsalingResource::collection($this->whenLoaded('products')),
+            "locale" => $locale
         ];
     }
 }
