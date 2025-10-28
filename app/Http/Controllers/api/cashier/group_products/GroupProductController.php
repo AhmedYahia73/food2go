9e502bee98e74382e23f0817b030724897599191
Ahmed <Wegostationdev@gmail.com>
2025-10-28 13:53:35 +0200

    commit

--- app/Http/Controllers/api/cashier/group_products/GroupProductController.php
+++ app/Http/Controllers/api/cashier/group_products/GroupProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\BranchOff;
 use App\Models\Product;
