ab458bb079ef4dea310d7c1aed9b81152718eadc
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-11-05 09:58:15 +0200

    commit

--- routes/client.php
+++ routes/client.php
@@ -21,6 +21,7 @@ Route::controller(ClientMakeOrderController::class)
 Route::controller(WaiterCallController::class)->group(function(){
     Route::post('/call_waiter', 'call_waiter');
     Route::post('/call_payment', 'call_payment');
+    Route::post('/cancel_call_pyment', 'cancel_call_pyment');
 });
 
 Route::controller(HomeController::class)->group(function(){
