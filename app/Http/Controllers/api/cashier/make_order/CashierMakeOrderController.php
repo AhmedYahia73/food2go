e68bdae1d2bd413187882fa96a9afd966d2f457f
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-11-08 16:03:03 +0200

    commit

--- app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
+++ app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
@@ -653,7 +653,9 @@ class CashierMakeOrderController extends Controller
         ->get();
         $orders = collect([]);
         foreach ($order_cart as $key => $item) {
-            $order_item = $this->order_format($item, $key); 
+            $order_item = $this->order_format($item, $key);
+            $amount = $order_item->price_after_discount + $order_item->price_after_tax - $order_item->price;
+            $amount *= floatval($order_item->count);
             $orders = $orders->merge($order_item);
         }
 
