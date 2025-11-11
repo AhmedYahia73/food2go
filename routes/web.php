00db952ff73e972f9c8a3653ce46ece3cc8bc89f
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-11 16:11:54 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Reverb\Facades\Reverb;
+
+Reverb::websocket('/', function(){
+    
+});
 
 Route::get('/', function () {
     return view('welcome');
