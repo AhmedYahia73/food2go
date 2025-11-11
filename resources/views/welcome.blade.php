f469d24231e2e9ee26c345a6c930d4a04241080a
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-11 16:43:36 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,24 +7,7 @@
     <!-- أولاً: مكتبة Pusher -->
 <script src="https://cdn.jsdelivr.net/npm/pusher-js@7.2.0/dist/web/pusher.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/laravel-echo@1.15.0/dist/echo.iife.js"></script>
-
-<script>
-const echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'foo2go123', // نفس المفتاح في .env
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-});
-
-echo.channel('print_order')
-    .listen('.print_order.printed', (data) => {
-        console.log("📡 Received:", data);
-    });
-</script>
->
+ 
 </head>
 <body>
     <h1>Reverb Test Page</h1> 
