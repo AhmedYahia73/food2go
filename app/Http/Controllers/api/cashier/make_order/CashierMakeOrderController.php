bab095bb33cff31e6338c2e2ad0ffff8e8c0d281
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-13 15:41:40 +0200

    comm

--- app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
+++ app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
@@ -1324,6 +1324,9 @@ class CashierMakeOrderController extends Controller
         $keyPath = 'qz/private-key.pem'; // المسار اللي جوه storage/app/
 
         try {
+            return response()->json([
+                'key' => Storage::get($keyPath)
+            ], 400);
             if (!Storage::exists($keyPath)) {
                 // اتأكد إن الملف موجود
                 return response('Private key not found.', 500)->header('Content-Type', 'text/plain');
