ea4826bc36dd77cc618e0323fe843232ea8b35c4
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-11-15 14:27:39 +0200

    commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\CustomerMiddleware;
