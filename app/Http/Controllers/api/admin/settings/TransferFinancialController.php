466ed369aa8c1090c9d9ea072f8e5a88c972025a
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-16 16:57:13 +0200

    commit

---
+++ app/Http/Controllers/api/admin/settings/TransferFinancialController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\admin\settings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TransferFinancialController extends Controller
+{
+    //
+}
