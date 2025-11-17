7e06c4ed0547b37d35e0733e7ce6ffd2d20f2f25
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-17 14:34:00 +0200

    commit

--- app/trait/PlaceOrder.php
+++ app/trait/PlaceOrder.php
@@ -864,36 +864,36 @@ trait PlaceOrder
         foreach ($item as $element) {
             $kitchen_item = KitchenItem::create([
                 "kitchen_order_id" => $kitchen_order->id,
-                "product_id" => $element['id'],
+                "product_id" => $element->id,
                // 'note' => $element->note,
             ]);
-            foreach ($element['extras'] as $value) {
+            foreach ($element->extras as $value) {
                 KItemExtra::create([
                     "kitchen_item_id" => $kitchen_item->id,
-                    "extra_id" => $value['id'],
+                    "extra_id" => $value->id,
                 ]);
             }
-            foreach ($element['excludes'] as $value) {
+            foreach ($element->excludes as $value) {
                 KItemExclude::create([
                     "kitchen_item_id" => $kitchen_item->id,
-                    "exclude_id" => $value['id'],
+                    "exclude_id" => $value->id,
                 ]);
             }
-            foreach ($element['addons_selected'] as $value) {
+            foreach ($element->addons_selected as $value) {
                 KItemAddon::create([
                     "kitchen_item_id" => $kitchen_item->id,
-                    "addon_id" => $value['id'],
+                    "addon_id" => $value->id,
                 ]);
             }
             foreach ($element->variation_selected as $value) {
                 $variation = KItemVriation::create([
                     "kitchen_item_id" => $kitchen_item->id,
-                    "variation_id" => $value['id'],
+                    "variation_id" => $value->id,
                 ]);
-                foreach ($value['options'] as $value_item) {
+                foreach ($value->options as $value_item) {
                     KItemOption::create([
                         "kitchen_variation_id" => $variation->id,
-                        "option_id" => $value_item['id'],
+                        "option_id" => $value_item->id,
                     ]);
                 }
             }
