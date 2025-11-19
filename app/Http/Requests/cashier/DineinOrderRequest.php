df722dbd5b30ddbde50f412efb5a198fa76d8107
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-11-19 16:17:11 +0200

    commit

--- app/Http/Requests/cashier/DineinOrderRequest.php
+++ app/Http/Requests/cashier/DineinOrderRequest.php
@@ -25,22 +25,22 @@ class DineinOrderRequest extends FormRequest
     {
         return [
      
-            'amount' => ['required_unless:order_pending,1,true', 'numeric'], 
-            'total_tax' => ['required_unless:order_pending,1,true', 'numeric'],
-            'total_discount' => ['required_unless:order_pending,1,true', 'numeric'], 
-            'products' => ['required_unless:order_pending,1,true', 'array'],
-            'products.*.product_id' => ['exists:products,id', 'required_unless:order_pending,1,true'],
+            'amount' => ['required', 'numeric'], 
+            'total_tax' => ['required', 'numeric'],
+            'total_discount' => ['required', 'numeric'], 
+            'products' => ['required', 'array'],
+            'products.*.product_id' => ['exists:products,id', 'required'],
             'products.*.exclude_id.*' => ['exists:exclude_products,id'],
             'products.*.extra_id.*' => ['exists:extra_products,id'],
-            'products.*.addons.*.addon_id' => ['exists:addons,id', 'required_unless:order_pending,1,true'],
-            'products.*.addons.*.count' => ['numeric', 'required_unless:order_pending,1,true'],
+            'products.*.addons.*.addon_id' => ['exists:addons,id', 'required'],
+            'products.*.addons.*.count' => ['numeric', 'required'],
             'products.*.variation.*.variation_id' => ['exists:variation_products,id'],
             'products.*.variation.*.option_id.*' => ['exists:option_products,id'],
-            'products.*.count' => ['numeric', 'required', 'required_unless:order_pending,1,true'],
+            'products.*.count' => ['numeric', 'required', 'required'],
             'products.*.note' => ['sometimes'], 
 
-            'products.*.price' => ['numeric', 'required_unless:order_pending,1,true'],
-            'products.*.addons.*.price' => ['numeric', 'required_unless:order_pending,1,true'],
+            'products.*.price' => ['numeric', 'required'],
+            'products.*.addons.*.price' => ['numeric', 'required'],
 
 
             'financials' => ['array'],
@@ -51,7 +51,7 @@ class DineinOrderRequest extends FormRequest
             'cashier_id' => ['required', 'exists:cashiers,id'],
             'user_id' => ['exists:users,id'],
             'dicount_id' => ['exists:discounts,id'],
-            'due' => ['required_unless:order_pending,1,true', 'boolean'],
+            'due' => ['required', 'boolean'],
             'source' => 'sometimes', 
             'table_id' => ['required', 'exists:cafe_tables,id'],
 
