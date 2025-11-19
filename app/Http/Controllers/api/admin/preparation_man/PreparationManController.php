ee3becfea32e64e30b231f0b8dbb1ca6ee23b3ee
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-11-19 09:37:12 +0200

    commit

--- app/Http/Controllers/api/admin/preparation_man/PreparationManController.php
+++ app/Http/Controllers/api/admin/preparation_man/PreparationManController.php
@@ -4,28 +4,130 @@ namespace App\Http\Controllers\api\admin\preparation_man;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\PreparationMan;
 
 class PreparationManController extends Controller
 {
-    public function __construct(){}
+    public function __construct(private PreparationMan $preparation_man){}
 
     public function view(Request $request){
+        $preparation_men = $this->preparation_man
+        ->with("branch")
+        ->get()
+        ->map(function($item){
+            return [
+                "id" => $item->id,
+                "name" => $item->name,
+                "status" => $item->status,
+                "branch" => [
+                    "id" => $item?->branch?->id,
+                    "name" => $item?->branch?->name,
+                ], 
+            ];
+        });
 
+        return response()->json([
+            'preparation_men' => $preparation_men
+        ]);
     }
 
-    public function man(Request $request){
+    public function preparation_man(Request $request, $id){
+        $preparation_man = $this->preparation_man
+        ->where("id", $id)
+        ->with("branch")
+        ->first();
 
+        return response()->json([
+            'data' => [ 
+                "id" => $preparation_man->id,
+                "name" => $preparation_man->name,
+                "status" => $preparation_man->status,
+                "branch" => [
+                    "id" => $preparation_man?->branch?->id,
+                    "name" => $preparation_man?->branch?->name,
+                ], 
+            ]
+        ]);
     }
 
-    public function status(Request $request){
+    public function status(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|boolean',
+        ]);
+        if ($validator->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validator->errors(),
+            ],400);
+        }
 
+        $this->preparation_man
+        ->where("id", $id)
+        ->update([
+            "status" => $request->status
+        ]);
+
+        return response()->json([
+            "success" => "You update status success"
+        ]);
     }
 
     public function create(Request $request){
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|boolean',
+            'password' => 'required',
+            'branch_id' => 'required|exists:branches,id',
+            'status' => 'required|boolean',
+        ]);
+        if ($validator->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validator->errors(),
+            ],400);
+        }
+        
+        $preparationRequest = $request->validated();
+        $this->preparation_man
+        ->create($preparationRequest);
 
+        return response()->json([
+            "success" => "You add data success"
+        ]);
     }
 
-    public function modify  (Request $request){
+    public function modify(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|boolean',
+            'password' => 'sometimes',
+            'branch_id' => 'required|exists:branches,id',
+            'status' => 'required|boolean',
+        ]);
+        if ($validator->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validator->errors(),
+            ],400);
+        }
+        
+        $preparationRequest = $request->validated();
+        if(!empty($request->password)){
+            $preparationRequest['password'] = bcrypt($request->password);
+        }
+        $this->preparation_man
+        ->where("id", $id)
+        ->update($preparationRequest);
 
+        return response()->json([
+            "success" => "You update data success"
+        ]);
+    }
+
+    public function delete(Request $request, $id){
+        $this->preparation_man
+        ->where("id", $id)
+        ->delete();
+
+        return response()->json([
+            "success" => "You delete data success"
+        ]);
     }
 }
--- app/Models/PreparationMan.php
+++ app/Models/PreparationMan.php
@@ -5,11 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class PreparationMan extends Model
-{  
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
     protected $fillable = [
         'name',
         'password',
         'branch_id',
         'status',
+    ];
+    protected $appends = ['role'];
+
+    public function getRoleAttribute(){
+        return 'preparation_man';
+    }
+
+    public function branch(){
+        return $this->belongsTo(Branch::class, "branch_id");
+    }
+    
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 }
