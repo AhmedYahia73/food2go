dcb5e1ff8ffb8b867f31241366cab2752399e9a8
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-11-30 14:51:27 +0200

    commit

--- app/trait/OrderFormat.php
+++ app/trait/OrderFormat.php
@@ -618,7 +618,7 @@ trait OrderFormat
                 "count_orders" => $order?->delivery?->count_orders ?? null,
             ],
             "address" => [
-                "id" => $order?->address?->id ?? null,
+                "id" => $order?->address,
                 "address" => $order?->address?->address ?? null,
                 "street" => $order?->address?->street ?? null,
                 "building_num" => $order?->address?->building_num ?? null,
