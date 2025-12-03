a866ca327bc7c76b513b2f880d90763e5be0da94
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-12-03 17:42:38 +0200

    commit

---
+++ app/Http/Controllers/api/admin/supplier/SupplierController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\api\admin\supplier;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    public function __construct(){}
+
+    public function view(){
+        
+    }
+
+    public function supplier(){
+        
+    }
+
+    public function create(){
+        
+    }
+
+    public function modify(){
+        
+    }
+
+    public function delete(){
+        
+    }
+}
