70ab0020e48cc78f818b49f413e694d1b1d37078
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-12-04 10:17:16 +0200

    commit

--- app/Http/Controllers/api/admin/supplier/SupplierController.php
+++ app/Http/Controllers/api/admin/supplier/SupplierController.php
@@ -4,28 +4,44 @@ namespace App\Http\Controllers\api\admin\supplier;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Supplier;
 
 class SupplierController extends Controller
 {
-    public function __construct(){}
+    public function __construct(private Supplier $suppliers){}
 
-    public function view(){
+    public function view(Request $request){
+        $suppliers = $this->suppliers
+        ->select("id", "name", "phone", "email", "status", "balance")
+        ->get();
+
+        return response()->json([
+            "suppliers" => $suppliers
+        ]);
+    }
+
+    public function supplier(Request $request, $id){
+        $supplier = $this->suppliers
+        ->select("id", "name", "phone", "email", "status", "balance")
+        ->where("id", $id)
+        ->first();
+
+        return response()->json([
+            "supplier" => $supplier
+        ]);
+    }
+
+    public function create(Request $request){
         
     }
 
-    public function supplier(){
+    public function modify(Request $request, $id){
         
     }
 
-    public function create(){
-        
-    }
-
-    public function modify(){
-        
-    }
-
-    public function delete(){
+    public function delete(Request $request, $id){
         
     }
 }
