13fcd938ee5c7bc012cc794f82ca23edda573d61
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2025-12-27 17:08:28 +0200

    commit

--- database/migrations/2025_12_27_145211_add_column_to_orders.php
+++ database/migrations/2025_12_27_145211_add_column_to_orders.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->foreignId('transfer_from_id')->nullable()->constrained('branches')->onUpdate('cascade')->onDelete('set null');
         });
     }
 
