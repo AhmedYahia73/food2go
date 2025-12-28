6c3947fb87471d945bb943e1d2706047cd48e048
AhmedYahia73 <ahmedahmadahmid73@exgmailample.com>
2025-12-28 16:11:43 +0200

    commit

--- app/Http/Controllers/api/admin/settings/fake_order/FakeOrderController.php
+++ app/Http/Controllers/api/admin/settings/fake_order/FakeOrderController.php
@@ -124,52 +124,11 @@ class FakeOrderController extends Controller
                 'errors' => $validator->errors(),
             ],400);
         }
-
-        $fake_order_password = Setting::
-        where("name", "fake_order_password")
-        ->first()?->setting ?? null;
-        $fake_order_precentage = Setting::
-        where("name", "fake_order_precentage")
-        ->first()?->setting ?? null;
-        $fake_order_limit = Setting::
-        where("name", "fake_order_limit")
-        ->first()?->setting ?? null;
+ 
         $fake_order_status = Setting::
         where("name", "fake_order_status")
-        ->first()?->setting ?? null;
-        if(empty($fake_order_password)){
-            Setting::create([
-                'name' => "fake_order_password",
-                'setting' => $request->fake_order_password,
-            ]);
-        }
-        else{
-            $fake_order_password->setting = $request->fake_order_password;
-            $fake_order_password->save();
-        }
-        
-        if(empty($fake_order_precentage)){
-            Setting::create([
-                'name' => "fake_order_precentage",
-                'setting' => $request->fake_order_precentage,
-            ]);
-        }
-        else{
-            $fake_order_precentage->setting = $request->fake_order_precentage;
-            $fake_order_precentage->save();
-        }
-        
-        if(empty($fake_order_limit)){
-            Setting::create([
-                'name' => "fake_order_limit",
-                'setting' => $request->fake_order_limit,
-            ]);
-        }
-        else{
-            $fake_order_limit->setting = $request->fake_order_limit;
-            $fake_order_limit->save();
-        }
-        
+        ->first()?->setting ?? null;   
+
         if(empty($fake_order_status)){
             Setting::create([
                 'name' => "fake_order_status",
