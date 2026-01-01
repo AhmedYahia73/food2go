fc3028b233efa77e16f85040aeeb8b7f6da63a59
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-01-01 15:00:40 +0200

    commit

--- app/Http/Controllers/api/admin/bundle/BundleController.php
+++ app/Http/Controllers/api/admin/bundle/BundleController.php
@@ -69,6 +69,8 @@ class BundleController extends Controller
             return [
                 "id" => $item->id,
                 "name" => $item->name,
+                "category_id" => $element->category_id,
+                "sub_category_id" => $element->sub_category_id,
                 "variations" => $item?->variations?->map(function($element){
                     return [
                         "id" => $element->id,
