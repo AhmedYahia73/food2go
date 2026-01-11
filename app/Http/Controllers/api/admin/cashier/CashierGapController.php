335836b7997c5e5f66ca72f2fba972b7e4c89b15
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-01-12 01:50:51 +0200

    commit

--- app/Http/Controllers/api/admin/cashier/CashierGapController.php
+++ app/Http/Controllers/api/admin/cashier/CashierGapController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api\admin\cashier;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\CashierGap;
 use App\Models\Cashier;
@@ -26,8 +27,26 @@ class CashierGapController extends Controller
     }
 
     public function cashier_gap(Request $request){
+        $validation = Validator::make($request->all(), [
+            'cashier_id' => 'exists:cashiers,id',
+            'cashier_man_id' => 'exists:cashier_men,id',
+        ]);
+        if ($validation->fails()) { // if Validate Make Error Return Message Error
+            return response()->json([
+                'errors' => $validation->errors(),
+            ],400);
+        }
         $gaps = CashierGap::
-        with("cashier", "cashier_man")
+        with("cashier", "cashier_man");
+        if($request->cashier_id){
+            $gaps = $gaps
+            ->where("cashier_id", $request->cashier_id);
+        }
+        if($request->cashier_man_id){
+            $gaps = $gaps
+            ->where("cashier_man_id", $request->cashier_man_id);
+        }
+        $gaps = $gaps
         ->get()
         ->map(function($item){
             $date = $item?->shift?->created_at ?? $item->created_at;
