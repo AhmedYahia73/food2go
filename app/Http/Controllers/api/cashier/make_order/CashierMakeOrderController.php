17f2acd85b6e60349c8cf02d51cd77a31e05786a
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-01-17 00:48:57 +0200

    commit

--- app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
+++ app/Http/Controllers/api/cashier/make_order/CashierMakeOrderController.php
@@ -959,7 +959,11 @@ class CashierMakeOrderController extends Controller
         foreach ($request->preparing as $value) {
             $order_cart = $this->order_cart
             ->where('id', $value['cart_id'])
+            ->where("prepration_status", "!=", "preparing")
             ->first();
+            if(empty($order_cart)){
+                continue;
+            }
             $preparing = $order_cart->cart;
             $order_cart->prepration_status = $value['status'];  
             $order_cart->save();
