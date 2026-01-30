0f5cdeb76d458f829abfe2ecb58f2ee2850e7d34
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-01-30 17:41:08 +0200

    commit

--- app/Http/Controllers/api/admin/report/ReportController.php
+++ app/Http/Controllers/api/admin/report/ReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\CashierMan;
 use App\Models\Cashier; 
