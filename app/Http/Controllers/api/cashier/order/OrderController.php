4a15408065ce4cfcaa948491f515a99d230a1992
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-02-13 17:07:55 +0200

    commit

--- app/Http/Controllers/api/cashier/order/OrderController.php
+++ app/Http/Controllers/api/cashier/order/OrderController.php
@@ -772,14 +772,30 @@ class OrderController extends Controller
     }
 
     public function order_count(Request $request){
-        
+   
         $time_sittings = $this->TimeSittings 
-        ->get();
+        ->get(); 
+        $items = [];
+        $count = 0;
+        $to = isset($time_sittings[0]) ? $time_sittings[0] : 0; 
+        $from = isset($time_sittings[0]) ? $time_sittings[0] : 0;
+        foreach ($time_sittings as $item) {
+            $items[$item->branch_id][] = $item;
+        }
+        foreach ($items as $item) {
+            if(count($item) > $count || (count($item) == $count && $item[count($item) - 1]->from > $to->from) ){
+                $count = count($item);
+                $to = $item[$count - 1];
+            } 
+            if($from->from > $item[0]->from){
+                $from = $item[0];
+            }
+        }
         if ($time_sittings->count() > 0) {
-            $from = $time_sittings[0]->from;
-            $end = date('Y-m-d') . ' ' . $time_sittings[$time_sittings->count() - 1]->from;
-            $hours = $time_sittings[$time_sittings->count() - 1]->hours;
-            $minutes = $time_sittings[$time_sittings->count() - 1]->minutes;
+            $from = $from->from;
+            $end = date('Y-m-d') . ' ' . $to->from;
+            $hours = $to->hours;
+            $minutes = $to->minutes;
             $from = date('Y-m-d') . ' ' . $from;
             $start = Carbon::parse($from);
             $end = Carbon::parse($end);
