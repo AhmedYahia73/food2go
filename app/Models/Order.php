fc39f947accd1a68a42b48a7cefd5edd5478b79d
Ahmed_Yahia73 <ahmedahmadahmid73@gmail.com>
2026-02-18 03:53:06 +0200

    dfht

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -85,103 +85,173 @@ class Order extends Model
         'order_active' // ده عشان لو مكملش طلب الاوردر يتحفظ فقط
     ];
     protected $appends = ['order_date', 'status_payment', 'order_details_data'];
-
-    protected $hidden = [
-        'pivot', 
-    ];
+ 
 
     public function financials(){
+        return $this->belongsToMany(FinantiolAcounting::class, 'order_financials', "order_id", "financial_id");
     }
 
     public function getdateAttribute(){
+        return $this->created_at 
+        ? $this->created_at->format('H:i:s') 
+        : null;
+
     }
 
     public function service_fees_item(){
+        return $this->belongsTo(ServiceFees::class, 'service_fees_id');
     }
     
     public function transfer_from(){
+        return $this->belongsTo(Branch::class, 'transfer_from_id');
     }
     
     public function bundles(){
+        return $this->hasMany(OrderBundle::class, 'bundle_id');
     }
 
     public function group_module(){
+        return $this->belongsTo(GroupProduct::class, 'module_id');
     }
 
-    public function getorderNumberAttribute()
-    {
+    public function getorderNumberAttribute(){
+        $time_settings = TimeSittings::
+        where('branch_id', $this->branch_id)
+        ->orderByDesc('id')
+        ->first();
+        if (empty($time_settings)) {
+            return $this->created_at->format('d') . $this->created_at->format('m') . 
+            $this->created_at->format('y') . $this->id;
+        }
+        else{
+            $from = $time_settings->from;
+            $to = $this->created_at->format('H:i:s');
+            if ($from > $to) {
+                $date = Carbon::parse($this->created_at)->subDay();
+            }
+            else{
+                $date = $this->created_at;
+            }
+            return $date->format('d') . $date->format('m') . 
+            $date->format('y') . $this->id;
+        }
     }
-
 
     public function getStatusPaymentAttribute(){
+        if (isset($this->attributes['status']) && $this->attributes['status'] == 1) {
+            return 'approved';
+        } 
+        elseif (!isset($this->attributes['status'])) { // Use isset to check if it's null or not set
+            return 'pending';
+        } 
+        elseif (isset($this->attributes['status']) && $this->attributes['status'] == 0) {
+            return 'rejected';
+        } 
+        elseif (isset($this->attributes['status']) && $this->attributes['status'] == 2) {
+            return 'faild';
+        } 
     }
     
     public function getOrderDateAttribute(){
+        if (isset($this->attributes['created_at'] )&& !empty($this->attributes['created_at'])) {
+            return $this->created_at
+            ? $this->created_at->format('Y-m-d')
+            : null;
+        } 
+        else {
+            return null;
+        }
     }
 
     public function getOrderDetailsDataAttribute(){
+        if(isset($this->attributes['order_details'])){
+            return json_decode($this->attributes['order_details'], true) ?? [];
+        }
+        return [];
     }
 
     public function getorderDetailsAttribute($data){
+        return json_decode($data);
     }
 
     public function void(){
+        return $this->belongsTo(VoidReason::class, 'void_id');
     }
 
     public function financial_accountigs(){
+        return $this->belongsToMany(FinantiolAcounting::class, 'order_financials', 'order_id', 'financial_id');
     }
 
     public function financial_amount(){
+        return $this->hasMany(OrderFinancial::class, 'order_id');
     }
 
     public function captain(){
+        return $this->belongsTo(CaptainOrder::class, 'captain_id');
     }
 
     public function delivery(){
+        return $this->belongsTo(Delivery::class, 'delivery_id');
     }
 
     public function table(){
+        return $this->belongsTo(CafeTable::class, 'table_id');
     }
 
     public function payment_method(){
+        return $this->belongsTo(PaymentMethod::class, 'payment_method_id');
     }
 
     public function user(){
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function branch(){
+        return $this->belongsTo(Branch::class, 'branch_id');
     }
 
     public function cashier_man(){
+        return $this->belongsTo(CashierMan::class, 'cashier_man_id');
     }
 
     public function casheir(){
+        return $this->belongsTo(Cashier::class, 'cashier_id');
     }
 
     public function products(){
+        return $this->belongsToMany(Product::class, 'order_product', 'order_id', 'product_id')
+        ->withPivot('created_at');
     }
 
     public function addons(){
+        return $this->belongsToMany(Addon::class, 'order_product', 'order_id', 'addon_id');
     }
 
     public function offers(){
+        return $this->belongsToMany(Offer::class, 'order_product', 'order_id', 'offer_id');
     }
 
     public function deal(){
+        return $this->belongsToMany(Deal::class, 'order_product', 'order_id', 'deal_id');
     }
 
     public function address(){
+        return $this->belongsTo(Address::class, 'address_id');
     }
 
     public function order_address(){
+        return $this->belongsTo(Address::class, 'address_id');
     }
 
-    public function admin(){ 
+    public function admin(){
+        return $this->belongsTo(Admin::class, 'admin_id');
     }
 
-    public function schedule(){ 
+    public function schedule(){
+        return $this->belongsTo(ScheduleSlot::class, 'sechedule_slot_id');
     }
 
-    public function details(){ 
+    public function details(){
+        return $this->hasMany(OrderDetail::class, 'order_id');
     }
 }
