81ec48ec9d5ef459944ffba106fbb9111059e650
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-03-17 05:55:29 +0200

    ؤخةةهف

---
+++ database/migrations/2026_02_14_202739_create_material_stores_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('material_stores', function (Blueprint $table) {
+            $table->dropForeign("material_stores_product_id_foreign");
+            $table->dropColumn("product_id");
+            $table->foreignId('product_id')->nullable()->constrained('purchase_products')->onUpdate('cascade')->onDelete('cascade');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('service_fees', function (Blueprint $table) {
+            //
+        });
+    }
+};
