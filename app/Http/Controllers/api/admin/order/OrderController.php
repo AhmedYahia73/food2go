be218e2db5a69f657a34b4848da6d3f7cd9205c5
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-04-14 21:36:23 +0200

    hgfdg

--- app/Http/Controllers/api/admin/order/OrderController.php
+++ app/Http/Controllers/api/admin/order/OrderController.php
@@ -1647,10 +1647,13 @@ class OrderController extends Controller
             'to_status' => $request->order_status,
         ]); 
         
+            $user_item = User::
+            where("id", $order->user_id )
+            ->first();
             return response()->json([
                 'order_status' => $request->order_status, 
                 "points" => $order->points,
-                "user_points" => $$user_item->points
+                "user_points" => $user_item->points
             ]);
         if($request->order_status == 'delivery'){ 
             $user_item = User::
