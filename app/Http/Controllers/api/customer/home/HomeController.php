aa9806832e6e65dc70e0a28bfedbe3de2d54d68e
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-04-17 01:44:18 +0200

    fdgfd

--- app/Http/Controllers/api/customer/home/HomeController.php
+++ app/Http/Controllers/api/customer/home/HomeController.php
@@ -835,8 +835,7 @@ class HomeController extends Controller
                 $product->favourite = $product->favourite_product->isNotEmpty();
                 $product->price = $product->product_pricing->first()?->price ?? $product->price;   
 
-                $tax_module = $product?->tax
-                ?->tax_module
+                $tax_module = $product?->tax_module
                 ?->map(function ($taxItem) use ($module, $branch_id, $product) {
 
                     $isFound = $taxItem->module
