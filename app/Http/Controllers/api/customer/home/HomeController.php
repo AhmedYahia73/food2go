a006b555475b27ba5aaf42eb27ba41afb588e119
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-04-17 01:54:45 +0200

    fdgrer

--- app/Http/Controllers/api/customer/home/HomeController.php
+++ app/Http/Controllers/api/customer/home/HomeController.php
@@ -838,7 +838,7 @@ class HomeController extends Controller
                 $tax_module = $product?->tax_module
                 ?->map(function ($taxItem) use ($module, $branch_id, $product) {
 
-                    return $taxItem;
+                    return $taxItem->module;
 
                 })
                 ->filter()
