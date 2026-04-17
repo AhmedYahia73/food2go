b704eb8639f86f348f9e21e0b58a8ec799589c58
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-04-17 02:00:05 +0200

    rftger

--- app/Http/Controllers/api/customer/home/HomeController.php
+++ app/Http/Controllers/api/customer/home/HomeController.php
@@ -843,7 +843,7 @@ class HomeController extends Controller
                 $tax_module = $product?->tax_module
                 ?->map(function ($taxItem) use ($module, $branch_id, $product) {
 
-                    $isFound = $taxItem->module;
+                    return $taxItem->module;
                     if($isFound->count() > 0){
                         return $product?->tax;
                     }
