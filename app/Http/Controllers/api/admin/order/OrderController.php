01005eb3a6a3191ae4806a77a03a8475c2b4df45
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-06-18 21:25:32 +0300

    fdgd

--- app/Http/Controllers/api/admin/order/OrderController.php
+++ app/Http/Controllers/api/admin/order/OrderController.php
@@ -1889,8 +1889,8 @@ class OrderController extends Controller
         if ($request->branch_id) {
             $orders = $orders->where('branch_id', $request->branch_id)->values();
         }
-
-        return response()->json(["hghjg"]);
+ 
+        return response()->json([$orders]);
         return response()->json([
             'orders' => $orders
         ]);
