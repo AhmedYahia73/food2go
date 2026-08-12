b1c507852975c940c507673af9f156f6d76d179a
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-12 12:26:27 +0300

    بل

--- routes/api.php
+++ routes/api.php
@@ -82,6 +82,6 @@ Route::prefix('user/auth')->group(function(){
 
 
 
-use App\Http\Controllers\Api\SyncController;
+use App\Http\Controllers\api\SyncController;
 Route::post('/sync/push', [SyncController::class, 'push']);
 Route::get('/sync/pull', [SyncController::class, 'pull']);
