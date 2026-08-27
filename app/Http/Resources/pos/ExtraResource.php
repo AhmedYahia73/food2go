7e8a174595d418bed10500af9e4201043175c3fc
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-27 07:56:50 +0300

    بيل

--- app/Http/Resources/pos/ExtraResource.php
+++ app/Http/Resources/pos/ExtraResource.php
@@ -22,35 +22,22 @@ class ExtraResource extends JsonResource
 
         $locale = app()->getLocale(); // Use the application's current locale
        if ($this->product?->taxes?->setting == 'included') {
-            $price_with_tax = $this->price;
-            
+            $price = $this->price;
             if (!empty($my_discount) && $my_discount->type == 'precentage') {
-                $discounted_price_with_tax = $price_with_tax - $my_discount->amount * $price_with_tax / 100;
-            } else {
-                $discounted_price_with_tax = $price_with_tax;
+                $discount = $price - $my_discount->amount * $price / 100;
             }
-
-            if (empty($this->product->tax)) {
-                $price_before_tax = $discounted_price_with_tax;
-                $tax_val = 0;
-            } else {
-                if ($this->product->tax->type == 'value') {
-                    $tax_val = 0; // Assuming value tax applies only to the base product
-                    $price_before_tax = $discounted_price_with_tax;
-                } else {
-                    $price_before_tax = $discounted_price_with_tax / (1 + ($this->product->tax->amount / 100));
-                    $tax_val = $discounted_price_with_tax - $price_before_tax;
-                }
+            else{
+                $discount = $price;
             }
-            
-            $base_price_before_tax = $price_before_tax + ($price_with_tax - $discounted_price_with_tax);
-
+            $price = empty($this->product->tax) ? $discount: 
+            ($this->product->tax->type == 'value' ? $discount 
+            : $discount + $this->product->tax->amount * $discount / 100);
+            $tax = $price;
             return [
                 'id' => $this->id,
-                'price_after_discount' => $price_before_tax,
-                'price_after_tax' => $discounted_price_with_tax,
-                'final_price' =>  $discounted_price_with_tax,
-                'tax_val' => round($tax_val, 2),
+                'price_after_discount' => $discount,
+                'price_after_tax' => $tax,
+                'final_price' =>  $tax,
                 'name' => TranslationTbl::where('key', $this->name)
                 ->where('locale', $locale)->first()?->value ?? $this->name,
                 'product_id' => $this->product_id,
@@ -58,7 +45,7 @@ class ExtraResource extends JsonResource
                 'option_id' => $this->option_id,
                 'min' => $this->min,
                 'max' => $this->max,
-                'price' => $base_price_before_tax,
+                'price' => $this->price,
             ]; 
         }
         else{
